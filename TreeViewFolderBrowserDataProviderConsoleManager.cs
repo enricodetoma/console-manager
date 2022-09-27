bda3a3314846e3460ce83a49ce4b4833b3d92067
Enrico <enrico.detoma@gmail.com>
2022-09-27 19:48:20 +0200

    TreeStrategyFolderBrowserProvider

---
+++ TreeViewFolderBrowserDataProviderConsoleManager.cs
@@ -0,0 +1,30 @@
+﻿using Raccoom.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_manager
+{
+    class TreeViewFolderBrowserDataProviderConsoleManager : TreeStrategyFolderBrowserProvider
+    {
+        private string _RootFolderConsoleManager;
+
+        public string RootFolderConsoleManager
+        {
+            get { return _RootFolderConsoleManager; }
+            set { _RootFolderConsoleManager = value; }
+        }
+
+        public override void RequestRootNode()
+        {
+            if (!string.IsNullOrEmpty(_RootFolderConsoleManager))
+            {
+                AttachSystemImageList(Helper);
+                // create root node with specified folder
+                this.CreateTreeNode(null, System.IO.Path.GetFileName(_RootFolderConsoleManager), _RootFolderConsoleManager, false, true, false);
+            }
+        }
+    }
+}
